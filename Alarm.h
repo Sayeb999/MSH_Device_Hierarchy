445b0d8cdcc783ef99eadeeb09e22537852377cb
Sayeb ul Malik <sayebulmalik@gmail.com>
2025-12-18 16:35:59 +0300

    Details and Comments for Alarm.h

--- Alarm.h
+++ Alarm.h
@@ -1,8 +1,18 @@
-/* File: Alarm.h */
+/* File: Alarm.h 
+ * Author: Sayeb Ul Malik
+ * Role: Device Hierarchy Implementation
+ * Date: December 2025
+ * Description: Defines the class structure and logic for this device type.
+ * Part of the Smart Home Automation System (MSH).
+ */
+
 #ifndef ALARM_H
 #define ALARM_H
 #include "Device.h"
 
+
+// Represents the master security alarm.
+// INHERITANCE: Inherits directly from Device.
 class Alarm : public Device {
 private:
     bool isActive;
@@ -12,7 +22,10 @@ public:
         isOn = true;
     }
 
-    // Safety Override
+    // SAFETY OVERRIDE:
+    // This function overrides the standard turnOff() to prevent 
+    // the alarm from being disabled manually by the user.
+    // Requirement LLR-024.
     void turnOff() {
         std::cout << "ERROR: Cannot turn off Alarm system manually!" << std::endl;
     }
